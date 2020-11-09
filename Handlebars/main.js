db0d95d6dfdb2f694836a599f11abc5e53ac81ee
issaZananiri <https://github.com/issaZananiri>
2020-11-10 01:45:16 +0200

    Handlebars done

---
+++ Handlebars/main.js
@@ -0,0 +1,24 @@
+
+$("button").on("click", function () {
+    let address = $("#addr-input").val()
+    let minPrice = $("#min-p-input").val()
+    let maxPrice = $("#max-p-input").val()
+    let minRooms = $("#min-r-input").val()
+    let maxRooms = $("#max-r-input").val()
+    let immediate = $("#immed-y")
+
+    let relevantApts = findRelevantApts(address, minPrice, maxPrice, minRooms, maxRooms, immediate)
+    renderApts(relevantApts)
+})
+const renderApts = function (apartments) {
+    $("#results").empty()
+    console.log(apartments) 
+     $("#results").empty()
+    console.log(apartments)
+    const ap = {apartments}
+    const source = $('#apart').html();
+    const template = Handlebars.compile(source);
+    const newHTML = template(ap);
+    $('#results').append(newHTML);
+}
+renderApts(apartments)
