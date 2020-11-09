fa1a337910abd8615dbe2c4fd5bb0e840d27fdf2
issaZananiri <https://github.com/issaZananiri>
2020-11-09 13:31:06 +0200

    JsonDone

---
+++ json/Ex3/Ex3.js
@@ -0,0 +1,53 @@
+
+let animal = {
+    "name": "Dog",
+    "Weight": 4,
+    "Endangered":"yes",
+    "Delicious ":3,
+    "home":"America",
+    "food": [
+      {
+        "name": "water",
+        "amount":30
+        
+      },
+      {
+        "name": "potatoes",
+        "amount":30
+        
+        
+      },
+      {
+        "name": "Banana",
+        "amount":30
+  
+        
+      },
+   
+    
+    ],
+    "Predators": [
+        {
+            "name": "Elephant "
+            
+            
+          },
+          {
+            "name": "Lion"
+            
+          },
+
+
+    ],
+    
+  }
+  
+  
+
+console.log(animal.name) 
+console.log(animal.Weight)
+console.log(animal.home) 
+console.log(animal.Predators[0].name)
+console.log(animal.Endangered)
+console.log(animal.food[0].name) 
+
---
+++ json/Ex4/Ex4.js
@@ -0,0 +1,53 @@
+
+let animal = {
+    "name": "Cow",
+    "Weight": 400,
+    "Endangered":"no",
+    "Delicious ":3,
+    "home":"World",
+    "food": [
+      {
+        "name": "water",
+        "amount":30
+        
+      },
+      {
+        "name": "potatoes",
+        "amount":10
+        
+        
+      },
+      {
+        "name": "Banana",
+        "amount":20
+  
+        
+      },
+   
+    
+    ],
+    "Predators": [
+        {
+            "name": "Elephant "
+            
+            
+          },
+          {
+            "name": "Lion"
+            
+          },
+
+
+    ],
+    
+  }
+  
+  
+
+console.log(animal.name) 
+console.log(animal.Weight)
+console.log(animal.home) 
+console.log(animal.Predators[1].name)
+console.log(animal.Endangered)
+console.log(animal.food[0].name) 
+
